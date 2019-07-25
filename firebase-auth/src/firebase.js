93a5b6d49f7b07387f36c16ea565ad2553bf6570
Luan Castheloge <luanccp@gmail.com>
2019-07-25 14:28:11 -0300

    [CORE] Firebase auth

---
+++ firebase-auth/src/firebase.js
@@ -0,0 +1,11 @@
+var app = {
+    apiKey: "AIzaSyCG1w6G3kHnpIZW7rRvo3V65HOO3UONlvE",
+    authDomain: "react-crud-ad9ac.firebaseapp.com",
+    databaseURL: "https://react-crud-ad9ac.firebaseio.com",
+    projectId: "react-crud-ad9ac",
+    storageBucket: "",
+    messagingSenderId: "832699671646",
+    appId: "1:832699671646:web:f248b01a7ab02116"
+  };
+
+  export default app;
