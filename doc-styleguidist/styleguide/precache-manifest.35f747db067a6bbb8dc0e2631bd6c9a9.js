53b3b0b805a9bb10a76d94a9e73c200428c71e93
Luan Castheloge <luanccp@gmail.com>
2019-07-25 18:47:24 -0300

    [CORE] Start with Styleguidist

---
+++ doc-styleguidist/styleguide/precache-manifest.35f747db067a6bbb8dc0e2631bd6c9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a3b9bfaffe92ecd76f",
+    "url": "build/2.f4a3b9bf.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "477d791ae1c5db356da5",
+    "url": "build/main.477d791a.js"
+  },
+  {
+    "revision": "f1dddd7eb95be5233acbbfd1d44ff3e3",
+    "url": "index.html"
+  },
+  {
+    "revision": "477d791ae1c5db356da5",
+    "url": "static/css/main.dac1fce0.chunk.css"
+  }
+]);
