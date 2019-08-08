2f9bda5ab327a7dda6326a8425e64eba484156cf
Luan Castheloge <luanccp@gmail.com>
2019-08-08 12:07:13 -0300

    [ADD] New project to create a slider component

---
+++ range-conmponent/src/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+
+const useStyles = makeStyles({
+  root: {
+    width: 350,
+  },
+  input: {
+    width: 42,
+  },
+});
+
+export default function InputSlider() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+
+  const PrettoSlider = withStyles({
+    root: {
+      color: '#52af77',
+      height: 8,
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus,&:hover,&$active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: 'calc(-50% + 4px)',
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+    },
+    rail: {
+      height: 8,
+      borderRadius: 4,
+    },
+  })(Slider);
+
+
+
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = event => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  // const handleBlur = () => {
+  //   if (value < 0) {
+  //     setValue(0);
+  //   } else if (value > 100) {
+  //     setValue(100);
+  //   }
+  // };
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+          <Input
+            className={classes.input}
+            value={value}
+            margin="dense"
+            onChange={handleInputChange}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+          <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
